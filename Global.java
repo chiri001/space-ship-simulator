915c6a7f0c7d4613f9f2e79c9a39825a3447b847
chiri001 <renniekipchirchir.com>
2023-10-06 13:48:53 -0400

    map+planet+pointer+ship

---
+++ Global.java
@@ -0,0 +1,6 @@
+
+
+public class Global {
+    public static int SCALE = 10; //scale constant for map drawings
+    public static int PADDING = 15; //padding variable for drawings
+}
--- MyMap.java
+++ MyMap.java
@@ -10,26 +10,31 @@
 
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 // setting up a drawing canvas interface for the map
 interface DrawingCanvas {
     /* draw 
-     * parameters include a 2d graphics and dimesnion
+     * parameters include a 2d graphics and dimensions of the drawing canvas
      * drawing interface for the file
+     * returns nothing
     */
     void draw(Graphics2D g, Dimension canvasSize);
 }
 
-//class that creates a circle drawing
+//class that creates a circle drawing using the drawing canvas interface
 class CircleCanvas implements DrawingCanvas {
     /* draw 
-     * parameters include a 2d graphics and dimesnion
-     * drawing interface for the file
+     * parameters include a 2d graphics and dimesnions of drawing canvas
+     * returns nothing
     */
     public void draw(Graphics2D g, Dimension canvasSize) {
-        int padding = 15;
-        int diameter = Math.min(canvasSize.width, canvasSize.height) - padding;
+        int diameter = Math.min(canvasSize.width, canvasSize.height) - Global.PADDING;
+
+        //centers the circle in the drawing panel
         int x = (canvasSize.width - diameter) / 2;
         int y = (canvasSize.height - diameter) / 2;
         g.drawOval(x, y, diameter, diameter);
@@ -38,31 +43,48 @@ class CircleCanvas implements DrawingCanvas {
 
 //SpaceShipCanvas Class that draws a spaceship
 class SpaceShipCanvas implements DrawingCanvas {
+
+    /* draw 
+     * parameters include a 2d graphics and dimesnions of drawing canvas
+     * returns nothing
+    */
     public void draw(Graphics2D g, Dimension canvasSize) {
+
         int baseSize = Math.min(canvasSize.width, canvasSize.height);
-        int spaceshipLength = baseSize / 3;  // Length of the horizontal base
-        int spaceshipHeight = spaceshipLength / 2;  // Height of the central triangle
 
-        int smallTriangleHeight = spaceshipHeight / 3;  // Height of the side triangles
+        //length of spaceship landing base
+        int spaceshipLength = baseSize / Global.SCALE; 
+        
+        //height for the main body for the spaceship
+        int spaceshipHeight = spaceshipLength / 2;
 
+        //height of the side compartments for a spaceship
+        int smallTriangleHeight = spaceshipHeight / 3; 
+
+        //TODO: randomize the division so that it can appear anywher on map
         int[] xPoints = {
-            canvasSize.width / 3  - spaceshipLength / 2,  // Leftmost point of the base
-            canvasSize.width / 3 - spaceshipLength / 4,  // Base of left triangle
-            canvasSize.width / 3,  // Top of the central triangle
-            canvasSize.width / 3 + spaceshipLength / 4,  // Base of right triangle
-            canvasSize.width / 3 + spaceshipLength / 2,  // Rightmost point of the base
-            canvasSize.width / 3 + spaceshipLength / 6,  // Bottom of the right triangle
-            canvasSize.width / 3 - spaceshipLength / 6   // Bottom of the left triangle
+            canvasSize.height / 3 + spaceshipHeight / 2,//base level
+            //top of left compartment
+            canvasSize.height / 3 + spaceshipHeight / 2 - smallTriangleHeight,
+
+            canvasSize.height / 3 - spaceshipHeight / 2,//height of ship
+
+            //top of left compartment
+            canvasSize.height / 3 + spaceshipHeight / 2 - smallTriangleHeight,
+            
+            canvasSize.height / 3 + spaceshipHeight / 2, //base level
+            canvasSize.height / 3 + spaceshipHeight / 2, //bottom of right
+            canvasSize.height / 3 + spaceshipHeight / 2 //bottom of left
         };
 
         int[] yPoints = {
-            canvasSize.height / 3 + spaceshipHeight / 2,  // Base level
-            canvasSize.height / 3 + spaceshipHeight / 2 - smallTriangleHeight,  // Top of left triangle
-            canvasSize.height / 3 - spaceshipHeight / 2,  // Tip of the central triangle
-            canvasSize.height / 3 + spaceshipHeight / 2 - smallTriangleHeight,  // Top of right triangle
-            canvasSize.height / 3 + spaceshipHeight / 2,  // Base level
-            canvasSize.height / 3 + spaceshipHeight / 2,  // Bottom of the right triangle
-            canvasSize.height / 3 + spaceshipHeight / 2   // Bottom of the left triangle
+            canvasSize.width / 3 - spaceshipLength / 2, //most left vertex
+            canvasSize.width / 3 - spaceshipLength / 4, //base of left side
+            canvasSize.width / 3, //top of main compartment
+            canvasSize.width / 3 + spaceshipLength / 4, //base of right side
+            canvasSize.width / 3 + spaceshipLength / 2, //most right vertex
+            canvasSize.width / 3 + spaceshipLength / 6, //bottom of right side
+            canvasSize.width / 3 - spaceshipLength / 6 //bottom of left side
         };
 
         g.fillPolygon(xPoints, yPoints, 7);
@@ -73,42 +95,121 @@ class SpaceShipCanvas implements DrawingCanvas {
 
 //SpaceShipCanvas Class that draws a spaceship
 class PlanetCanvas implements DrawingCanvas {
+    
+    /* draw 
+     * parameters include a 2d graphics and dimesnions of drawing canvas
+     * returns nothing
+    */
     public void draw(Graphics2D g, Dimension canvasSize) {
-        int diameter = (Math.min(canvasSize.width, canvasSize.height) / 5);
+        int diameter = (Math.min(canvasSize.width, 
+                        canvasSize.height) / Global.SCALE); //planet diameter
+        
+        //calculating x and y coordinate of planet
+        //TODO: randomize the division
         int x = (int) ((canvasSize.width - diameter) / 1.5);
         int y = (int) ((canvasSize.height - diameter) / 1.5);
+
         g.setColor(Color.gray);
         g.fillOval(x, y, diameter, diameter);
     }
 }
 
-//SpaceShipCanvas Class that draws a spaceship
-// class pointerCanvas implements DrawingCanvas {
-//     public void draw(Graphics2D g, Dimension canvasSize) {
-//         g.filLine(35, 45, 75, 95);
-//     }
-// }
+//pointer hand to simulate a rotating radar
+class PointerCanvas implements DrawingCanvas {
 
+    private double pointerAngle = 0; //variable to track angle of rotation
 
-// Step 4: MyMap Class
+    /* draw 
+     * parameters include a 2d graphics and dimesnions of drawing canvas
+     * returns nothing
+    */
+    public void draw(Graphics2D g, Dimension canvasSize) {
+
+        //x and y coordinates and diameterof the circle map
+        int diameter = Math.min(canvasSize.width, canvasSize.height) - Global.PADDING;
+        int x = (canvasSize.width - diameter) / 2;
+        int y = (canvasSize.height - diameter) / 2;
+        
+
+        //center of the circle map should be starting point for pointer
+        int centerX = x + diameter / 2;
+        int centerY = y + diameter / 2;
+
+        //end of the circle map using the formula x = r * cos(angle)
+        //using angles so that it is recalculated to simulate rotation of ptr
+        int endX = centerX + (int) (diameter / 2 * Math.cos(pointerAngle - Math.PI / 2));
+        int endY = centerY + (int) (diameter / 2 * Math.sin(pointerAngle - Math.PI / 2));
+        
+        g.setColor(Color.green);
+        g.drawLine(centerX, centerY, endX, endY);
+    }
+
+    /* updatePointer
+     * parameters none
+     * returns nothing
+     * The function updates the pointer angle every time it is called
+    */
+    public void updatePointer() {
+        pointerAngle += Math.toRadians(2); //update angle by 2 degrees
+    }
+}
+
+/*class that implements the rotation aspect of the pointer */
+class PointerUpdateListener implements ActionListener {
+    
+    private PointerCanvas pointerCanvas;
+    private MyMap myMap;
+
+    /* PointerUpdateListener
+     * parameters include the pointer hand and the parent container i.e map
+     * returns nothing
+     * constructor for the class
+    */
+    public PointerUpdateListener(PointerCanvas pCanvas, MyMap map) {
+        this.pointerCanvas = pCanvas;
+        this.myMap = map;
+    }
+
+    /*implements the rotation action but relies on PointerCanvas class fn()
+    */
+    public void actionPerformed(ActionEvent e) {
+        pointerCanvas.updatePointer();
+        myMap.repaint(); //redraw the entire map
+    }
+}
+
+//My map class that is incharge of handling elements in the map
 class MyMap extends JPanel {
-    private CircleCanvas circleCanvas;
-    private SpaceShipCanvas spaceShipCanvas;
-    private PlanetCanvas planetCanvas;
+    private CircleCanvas circleCanvas; //circle boundary for map
+    private SpaceShipCanvas spaceShipCanvas; //spaceship drawing class
+    private PlanetCanvas planetCanvas;//planet drawing class
+    private PointerCanvas pointerCanvas; //pointer hand drawing class
 
     public MyMap() {
+        //create drawings
         this.circleCanvas = new CircleCanvas();
         this.spaceShipCanvas = new SpaceShipCanvas();
         this.planetCanvas = new PlanetCanvas();
+        this.pointerCanvas = new PointerCanvas();
+        Timer timer = new Timer(50, 
+                                new PointerUpdateListener(pointerCanvas, this));
+        timer.start();
     }
 
+    /* PointerUpdateListener
+     * parameters include the pointer hand and the parent container i.e map
+     * returns nothing
+     * constructor for the class
+    */
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D graphic_2d = (Graphics2D) g; //2d graphics for drawings
 
-        circleCanvas.draw(g2d, getSize());
-        spaceShipCanvas.draw(g2d, getSize());
-        planetCanvas.draw(g2d, getSize());
+        //draw shapes using draw function in DrawingCanvas interface
+        circleCanvas.draw(graphic_2d, getSize());
+        spaceShipCanvas.draw(graphic_2d, getSize());
+        planetCanvas.draw(graphic_2d, getSize());
+        pointerCanvas.draw(graphic_2d, getSize());
     }
 }
 
